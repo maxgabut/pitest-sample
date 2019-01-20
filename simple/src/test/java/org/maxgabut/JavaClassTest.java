b4fe394dd662f71c8c0d677ecf8968689a6ed624
Maxime Gabut <maxime.gabut@gmail.com>
2019-01-20 21:46:29 +0100

    Change test in "simple" to kill mutant

--- simple/src/test/java/org/maxgabut/JavaClassTest.java
+++ simple/src/test/java/org/maxgabut/JavaClassTest.java
@@ -2,9 +2,12 @@ package org.maxgabut;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class JavaClassTest {
 
     @Test
     public void getLol() {
+      assertEquals("lol", new JavaClass().getLol());
     }
 }
