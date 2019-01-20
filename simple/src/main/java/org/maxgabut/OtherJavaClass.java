9935785ace536ebdc5e7757c44ce45a4b0cdacee
Maxime Gabut <maxime.gabut@gmail.com>
2019-01-20 18:50:47 +0100

    Add profiles using exclusion to "simple" project

---
+++ simple/src/main/java/org/maxgabut/OtherJavaClass.java
@@ -0,0 +1,10 @@
+package org.maxgabut;
+
+import java.util.NoSuchElementException;
+
+public class OtherJavaClass {
+
+  public String getFreeBeer() {
+    throw new NoSuchElementException();
+  }
+}
